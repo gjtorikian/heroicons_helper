6f12c6e1566ea38a90564c5fb3ed5031a6451d3c
Garen J. Torikian <gjtorikian@users.noreply.github.com>
2022-06-11 10:28:30 -0400

    steal more things

--- lib/heroicons_helper/icon.rb
+++ lib/heroicons_helper/icon.rb
@@ -3,22 +3,29 @@
 module HeroiconsHelper
   # Icon to show heroicons by name and variant.
   class Icon
-    attr_reader :path, :attributes, :width, :height, :symbol, :variant
+    attr_reader :path, :attributes, :width, :height, :symbol, :variant, :keywords
 
     VARIANT_OUTLINE = "outline"
     VARIANT_SOLID = "solid"
     VARIANTS = [VARIANT_OUTLINE, VARIANT_SOLID].freeze
 
     def initialize(symbol, variant, attributes: {})
-      @symbol = symbol
-      @variant = variant
+      @symbol = symbol.to_s
+      @variant = variant.to_s
 
-      icon = get_heroicon(@symbol, @variant)
+      heroicon = get_heroicon(@symbol, @variant)
 
-      @path = icon["path"]
-      @width = icon["width"]
-      @height = icon["height"]
-      @attributes = attributes.dup.merge!(default_attributes)
+      @path = heroicon["path"]
+      @width = heroicon["width"]
+      @height = heroicon["height"]
+      @keywords = heroicon["keywords"]
+      @attributes = attributes.dup
+      @attributes[:class] = classes
+      @attributes[:viewBox] = viewbox
+      @attributes.merge!(size)
+      @attributes[:version] = "1.1"
+      @attributes.merge!(variant_attributes)
+      @attributes.merge!(a11y)
     end
 
     # Returns an string representing a <svg> tag
@@ -26,54 +33,93 @@ module HeroiconsHelper
       "<svg xmlns=\"http://www.w3.org/2000/svg\" #{html_attributes}>#{@path}</svg>"
     end
 
-    private
-
-    def html_attributes
+    private def html_attributes
       attrs = []
       @attributes.each_pair { |attr, value| attrs << "#{attr}=\"#{value}\"" }
       attrs.map(&:strip).join(" ")
     end
 
-    def default_attributes
+    # add some accessibility features to svg
+    private def a11y
+      accessible = {}
+
+      if @attributes[:"aria-label"].nil? && @attributes["aria-label"].nil?
+        accessible[:"aria-hidden"] = "true"
+      else
+        accessible[:role] = "img"
+      end
+
+      accessible
+    end
+
+    # prepare the octicon class
+    private def classes
+      "heroicon heroicon-#{@symbol}-#{@variant} #{@attributes[:class]} ".strip
+    end
+
+    private def variant_attributes
       case @variant
       when VARIANT_OUTLINE
         {
-          viewBox: viewbox,
           fill: "none",
           stroke: "currentColor",
         }
       when VARIANT_SOLID
         {
-          viewBox: viewbox,
           fill: "currentColor",
         }
       end
     end
 
-    def viewbox
+    private def viewbox
       "0 0 #{@width} #{@height}"
     end
 
-    def get_heroicon(symbol, variant)
-      symbol_s = symbol.to_s
-      raise ArgumentError, "Icon name can't be empty" if symbol_s.empty?
+    # determine the height and width of the octicon based on :size option
+    private def size
+      size = {
+        width: @width,
+        height: @height,
+      }
 
-      variant_s = variant.to_s
-      raise ArgumentError, "Variant `#{variant.inspect}` is invalid; must be one of #{VARIANTS.join(", ")}" unless VARIANTS.include?(variant_s)
+      # Specific size
+      unless @attributes[:width].nil? && @attributes[:height].nil?
+        size[:width]  = @attributes[:width].nil? ? calculate_width(@attributes[:height]) : @attributes[:width]
+        size[:height] = @attributes[:height].nil? ? calculate_height(@attributes[:width]) : @attributes[:height]
+        @width = size[:width]
+        @height = size[:height]
+      end
 
-      icon = HeroiconsHelper::ICON_SYMBOLS[symbol_s]
+      size
+    end
+
+    private def calculate_width(height)
+      (height.to_i * @width) / @height
+    end
+
+    private def calculate_height(width)
+      (width.to_i * @height) / @width
+    end
+
+    private def get_heroicon(symbol, variant)
+      raise ArgumentError, "Icon name can't be empty" if symbol.empty?
+
+      raise ArgumentError, "Variant `#{variant.inspect}` is invalid; must be one of #{VARIANTS.join(", ")}" unless VARIANTS.include?(variant)
+
+      icon = HeroiconsHelper::ICON_SYMBOLS[symbol]
 
       raise ArgumentError, "Couldn't find Heroicon for `#{symbol.inspect}`" unless icon
 
-      icon_in_variant = icon["variants"][variant_s]
-      raise ArgumentError, "Heroicon for `#{symbol.inspect}` doesn't have variant `#{variant.inspect}`" unless icon_in_variant
+      icon_variant = icon["variants"][variant]
+      raise ArgumentError, "Heroicon for `#{symbol.inspect}` doesn't have variant `#{variant.inspect}`" unless icon_variant
 
       {
         "name" => icon["name"],
-        "variant" => variant_s,
-        "width" => icon_in_variant["width"],
-        "height" => icon_in_variant["height"],
-        "path" => icon_in_variant["path"],
+        "variant" => variant,
+        "keywords" => icon["keywords"] || [],
+        "width" => icon_variant["width"],
+        "height" => icon_variant["height"],
+        "path" => icon_variant["path"],
       }
     end
   end
--- test/icon_test.rb
+++ test/icon_test.rb
@@ -3,18 +3,18 @@
 require "test_helper"
 
 describe HeroiconsHelper::Icon do
-  it "initialize accepts a string for an icon with variant" do
-    icon = heroicon("x", "outline")
+  it "initialize accepts a string for an icon" do
+    icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID)
     assert icon
   end
 
-  it "initialize accepts a symbol for an icon with variant" do
-    icon = heroicon(:x, "outline")
+  it "initialize accepts a symbol for an icon" do
+    icon = heroicon(:x, variant: HeroiconsHelper::Icon::VARIANT_SOLID)
     assert icon
   end
 
   it "the attributes are readable" do
-    icon = heroicon("x", "outline")
+    icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_OUTLINE)
     assert icon.path
     assert icon.attributes
     assert_equal "x", icon.symbol
@@ -23,20 +23,25 @@ describe HeroiconsHelper::Icon do
     assert_equal 24, icon.height
   end
 
+  it "gets keywords for the icon" do
+    icon = heroicon("archive", variant: HeroiconsHelper::Icon::VARIANT_OUTLINE)
+    assert_equal ["box", "catalog"], icon.keywords
+  end
+
   it "errors on invalid name" do
     assert_raises ArgumentError do
-      heroicon(:not_exist, "outline")
+      heroicon(:not_exist, variant: HeroiconsHelper::Icon::VARIANT_OUTLINE)
     end
 
     assert_raises ArgumentError do
-      heroicon(nil, "solid")
+      heroicon(nil, variant: HeroiconsHelper::Icon::VARIANT_SOLID)
     end
 
     assert_raises ArgumentError do
-      heroicon(25, "solid")
+      heroicon(25, variant: HeroiconsHelper::Icon::VARIANT_SOLID)
     end
     assert_raises ArgumentError do
-      refute heroicon([], "solid")
+      refute heroicon([], variant: HeroiconsHelper::Icon::VARIANT_SOLID)
     end
   end
 
@@ -56,19 +61,108 @@ describe HeroiconsHelper::Icon do
 
   describe "viewBox" do
     it "always has a viewBox" do
-      outline_icon = heroicon("x", "outline")
+      outline_icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_OUTLINE)
       assert_includes outline_icon.to_svg, "viewBox=\"0 0 24 24\""
-      solid_icon = heroicon("x", "solid")
+      assert_includes outline_icon.to_svg, "fill=\"none\""
+      assert_includes outline_icon.to_svg, "stroke=\"currentColor\""
+      solid_icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID)
       assert_includes solid_icon.to_svg, "viewBox=\"0 0 20 20\""
+      assert_includes solid_icon.to_svg, "fill=\"currentColor\""
     end
   end
 
   describe "html_attributes" do
     it "includes other html attributes" do
-      icon = heroicon("x", "solid", attributes: { class: "class1 class2", foo: "bar", disabled: "true" })
-      assert_includes icon.to_svg, "class=\"class1 class2\""
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, foo: "bar", disabled: "true")
       assert_includes icon.to_svg, "disabled=\"true\""
       assert_includes icon.to_svg, "foo=\"bar\""
+    end
+  end
+
+  describe "classes" do
+    it "includes classes passed in" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, class: "text-closed")
+      assert_includes icon.to_svg, "class=\"heroicon heroicon-x-solid text-closed\""
+    end
+  end
+
+  describe "size" do
+    it "always has width and height" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID)
+      assert_includes icon.to_svg, "height=\"20\""
+      assert_includes icon.to_svg, "width=\"20\""
+    end
+
+    it "converts number string height to integer" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, height: "60")
+      assert_includes icon.to_svg, "height=\"60\""
+      assert_includes icon.to_svg, "width=\"60\""
+    end
+
+    it "converts number height to integer" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, height: 60)
+      assert_includes icon.to_svg, "height=\"60\""
+      assert_includes icon.to_svg, "width=\"60\""
+    end
+
+    it "converts number string width to integer" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, width: "45")
+      assert_includes icon.to_svg, "height=\"45\""
+      assert_includes icon.to_svg, "width=\"45\""
+    end
+
+    it "converts number width to integer" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, width: 45)
+      assert_includes icon.to_svg, "height=\"45\""
+      assert_includes icon.to_svg, "width=\"45\""
+    end
+
+    it "with height and width passed in" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, width: 60, height: 60)
+      assert_includes icon.to_svg, "width=\"60\""
+      assert_includes icon.to_svg, "height=\"60\""
+    end
+
+    it "chooses the correct svg given a height" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, height: 32)
+      assert_includes icon.to_svg, "width=\"32\""
+      assert_includes icon.to_svg, "height=\"32\""
+      assert_includes icon.to_svg, "viewBox=\"0 0 20 20\""
+    end
+
+    it "chooses the correct svg given a width" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, width: 48)
+      assert_includes icon.to_svg, "width=\"48\""
+      assert_includes icon.to_svg, "height=\"48\""
+      assert_includes icon.to_svg, "viewBox=\"0 0 20 20\""
+    end
+
+    it "chooses the correct svg given a height and width" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, height: 24, width: 16)
+      assert_includes icon.to_svg, "width=\"16\""
+      assert_includes icon.to_svg, "height=\"24\""
+      assert_includes icon.to_svg, "viewBox=\"0 0 20 20\""
+    end
+  end
+
+  describe "a11y" do
+    it "includes attributes for symbol keys" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, "aria-label": "Close")
+      assert_includes icon.to_svg, "role=\"img\""
+      assert_includes icon.to_svg, "aria-label=\"Close\""
+      refute_includes icon.to_svg, "aria-hidden"
+    end
+
+    it "includes attributes for string keys" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID, "aria-label" => "Close")
+      assert_includes icon.to_svg, "role=\"img\""
+      assert_includes icon.to_svg, "aria-label=\"Close\""
+      refute_includes icon.to_svg, "aria-hidden"
+    end
+
+    it "has aria-hidden when no label is passed in" do
+      icon = heroicon("x", variant: HeroiconsHelper::Icon::VARIANT_SOLID)
+      assert_includes icon.to_svg, "aria-hidden=\"true\""
     end
   end
 end
